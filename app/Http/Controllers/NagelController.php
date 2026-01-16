5bbd4a0d2159a56f9e6c8a1e0ff4d3983f300488
forcemepls <grishin.egor.2004@mail.ru>
2026-01-16 18:16:54 +0300

    Add: Nagel Schreckenberg model without GUI

--- app/Http/Controllers/NagelController.php
+++ app/Http/Controllers/NagelController.php
@@ -3,27 +3,77 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\NagelSchreckenbergService;
 
 class NagelController extends Controller
 {
 
-    public function speedup(){  // 1. ускорение
-        
-    }
-
-    public function slowdown(){  // 2. торможение
-
-    }
-
-    public function random(){  // 3. случайное событие
-
-    }
-
-    public function move(){  // 4. движение
-
-    }
-
     public function main(){
-        
+
+        $roadLength = 15;
+        $iterations = 10;
+        $vMax = 4;
+        $ca = [];
+
+        for ($i = 0; $i < $roadLength; $i++){
+            $ca[] = 0;
+        }
+
+        $service = new NagelSchreckenbergService();
+
+        $machines = [
+            [
+                'id' => 1,
+                'speed' => 0,
+                'position' => 13,
+            ],
+            [
+                'id' => 0,
+                'speed' => 0,
+                'position' => 2,
+            ],
+            [
+                'id' => 2,
+                'speed' => 0,
+                'position' => 9,
+            ],
+        ];
+
+        $newState = $service->calculateStep($machines, $roadLength, $iterations, $vMax);
+
+        $history = $service->calculateStep($machines, $roadLength, $iterations, $vMax);
+
+        // --- БЛОК ОТЛАДКИ (Визуализация в браузере) ---
+        echo "<pre style='font-family: monospace; font-size: 16px; line-height: 1.5;'>";
+        echo "Road Length: {$roadLength}, Max Speed: {$vMax}<br><br>";
+
+        foreach ($history as $stepIndex => $stepData) {
+            // 1. Создаем пустую дорогу
+            // array_fill создает массив из точек ['.', '.', '.', ...]
+            $roadVisual = array_fill(0, $roadLength, '.');
+
+            // 2. Расставляем машины
+            foreach ($stepData as $car) {
+                // Если машина на этой позиции, ставим её ID (или 'X', если наложение)
+                $pos = $car['position'];
+                $id = $car['id'];
+                
+                // Проверка на аварию (для отладки): если в клетке уже не точка
+                if ($roadVisual[$pos] !== '.') {
+                    $roadVisual[$pos] = 'CRASH'; 
+                } else {
+                    $roadVisual[$pos] = "<b>{$id}</b>"; // Жирный шрифт для машины
+                }
+            }
+
+            // 3. Выводим строку
+            echo "Step {$stepIndex}: [" . implode(' ', $roadVisual) . "] <br>";
+            
+            // Дополнительно можно вывести параметры машин текстом
+            // foreach ($stepData as $car) { echo "(ID:{$car['id']} v:{$car['speed']} x:{$car['position']}) "; }
+            // echo "<br><br>";
+        }
+        echo "</pre>";
+        die(); // Останавливаем выполнение, чтобы Laravel не пытался рендерить View
     }
 }
---
+++ app/Services/NagelSchreckenbergService.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Services;
+
+class NagelSchreckenbergService
+{
+
+    protected function speedup(int $currentSpeed, int $vMax): int{  // 1. ускорение
+        return min($currentSpeed + 1, $vMax);
+    }
+
+    protected function slowdown(int $currentSpeed, int $gap): int{  // 2. торможение
+        return  min($currentSpeed, $gap);
+    }
+
+    protected function random(int $currentSpeed, float $p): int{  // 3. случайное событие
+        if ($currentSpeed > 0 && (rand(0, 100) / 100) < $p){
+            return $currentSpeed - 1;
+        }
+        return $currentSpeed;
+    }
+
+    protected function move(int $position, int $speed, int $roadLength){  // 4. движение
+        $newPos = $position + $speed;
+
+        if ($newPos >= $roadLength) {
+            $newPos = $newPos - $roadLength;
+        }
+
+        return $newPos;
+    }
+
+    public function calculateStep(array $initialState, int $roadLength, int $iterations, int $vMax){
+        $history = [];  // здесь будет состояние дороги на каждом шаге
+
+        $currentMachines = $initialState;  // текущее состояние машин
+
+        $history[] = $currentMachines;  // текущее состояние в историю
+
+        for ($t = 0; $t < $iterations; $t++){
+            
+            // Сортировка по месту в КА
+            usort($currentMachines, function($a, $b) {
+                return $a['position'] <=> $b['position'];
+            });
+
+            $nextMachinesState = [];
+            $totalCars = count($currentMachines);
+
+            for ($i = 0; $i < $totalCars; $i++){
+
+                $machine = $currentMachines[$i];
+
+                // определение индекса лидера
+                // ели текущая машина не последняя - (%i + 1)
+                // если последнияя - беру первого (0) - замыкаю кольцо
+                $leaderIndex = ($i + 1) % $totalCars;
+                $leader = $currentMachines[$leaderIndex];
+
+                $v = $machine['speed'];
+                $v = $this->speedup($v, $vMax);
+
+                if ($leaderIndex == 0){
+                    $gap = ($roadLength - $machine['position']) + $leader['position'] - 1;
+                } else{
+                    $gap = $leader['position'] - $machine['position'] - 1;
+                }
+
+                $v = $this->slowdown($v, $gap);
+
+                $v = $this->random($v, 0.3);
+
+                $nextMachinesState[] = [
+                    'id' => $machine['id'],
+                    'speed' => $v,
+                    'position' => $machine['position'],
+                ];
+            }
+
+            foreach ($nextMachinesState as $key => $m) {
+                $nextMachinesState[$key]['position'] = $this->move($m['position'], $m['speed'], $roadLength);
+            }
+
+            $history[] = $nextMachinesState;
+            $currentMachines = $nextMachinesState;
+        }
+
+        return $history;
+    }
+}
